accadd95d8e4655d8186d1b94f38f2a50ac73895
Scott J. Roberts <Scott.John.Roberts@ibm.com>
2018-03-12 15:30:05 -0400

    wrote the help page for users

--- components/presentationals/SchedulerHelper.js
+++ components/presentationals/SchedulerHelper.js
@@ -7,6 +7,48 @@ import PropTypes from 'prop-types';
 
 const initialSource = `
 ## How to schedule yourself
+In order to use this you need:
+		A code from Kalamazoo Loaves and Fishes
+		A stable internet connection 
+		Self-scheduling privileges
+		
+If you do not have any of these things, call into Kalamazoo Loaves and Fishes
+
+										WALKTHROUGH
+
+											Step1: 
+Enter your house code into the given field (1234 is the example here, yours will look different)
+If your house code does not work, check your input it needs to be exactly what you were given from Kalamazoo Loaves and Fishes
+<img src = "/img/Step1.png" height = "500" >
+
+
+											Step2: 
+Enter the information for your head of household (First Name, Last Name, and Date of Birth)										
+<img src = "/img/Step2.png" height = "500" >
+
+If you see an error message, you haven't entered the head of household correctly. Double check the infromation
+is correct and then if errors persist try other members of your house. If errors still continue, call into Kalamazoo
+Loaves and Fishes to schedule
+<img src = "/img/Step2a.png" height = "500" >
+
+
+											Step3: 
+Once you have entered the correct information it will take you to the scheduling page
+Below select your pantry and click the schedule button to reserve your slots 
+
+<img src = "/img/Step3.png" height = "500" >
+
+
+											FAQ:
+
+WHY AM I NOT SEEING EVERY PANTRY?
+ Only pantries with available capacity to fit your household are displayed
+ 
+HOW DO I GET A CODE?
+	Call into Kalamazoo Loaves and Fishes to see if you are eligible
+	
+I CANT LOG IN
+	Double check that your input is correct and there are no typos, if problems still persist call KLF and schedule manually
 `
 
 /**
@@ -25,7 +67,7 @@ class HelperContainer extends React.Component {
 		render() {
 
 			const output = '## this is a double header thing'
-			return <ReactMarkdown source = {initialSource} />
+			return <ReactMarkdown escapeHtml = {false} source = {initialSource} />
 
 		}
 }
