962d095d169ede5586825e89aa547da217db24a0
Joshua Gibson <gibsonjoshua55@gmail.com>
2018-03-12 15:59:07 -0400

    Added documentation to the home page

--- components/presentationals/HomePage.js
+++ components/presentationals/HomePage.js
@@ -6,7 +6,23 @@ class HomePage extends React.Component {
   render()
   {
     return (
-      <Typography variant="headline" > Welcome to our Home Page </Typography>
+      <div>
+        <Typography variant="headline" > Welcome to our the Kalamazoo Loaves and Fishes Scheduler </Typography> <br/>
+        <Typography variant="subheading" >
+          If you are a client trying to register, either choose the Scheduler from the sidebar or 
+          click on the "How to scheudle yourself" link.
+        </Typography><br/>
+        <Typography variant="subheading" >
+          If you are a KLF staff member or volunteer, please log in.
+        </Typography> <br/>
+        <Typography variant="headline" >
+          Note: For this prototype, the login in for KLF Staff is: 
+        </Typography>
+        <Typography variant="title" style={{marginLeft: "20px"}} >
+          Username: admin, Password: password
+        </Typography>
+      </div>
+
     )
   }
 }
